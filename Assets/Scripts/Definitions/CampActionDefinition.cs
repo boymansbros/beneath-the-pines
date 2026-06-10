ab7d59061441032dc80e1fe36fbdaa3f4661f160
boymansbros <boymansbros@gmail.com>
2026-06-10 16:47:17 -0400

    Add CampActionDefinition class
    Define properties for camp actions including costs and recovery bonuses.

---
+++ Assets/Scripts/Definitions/CampActionDefinition.cs
@@ -0,0 +1,18 @@
+namespace BeneathThePines
+{
+    public class CampActionDefinition
+    {
+        public string ActionId;
+        public string DisplayName;
+
+        public bool IsMandatory;
+
+        public int DaylightCost;
+        public int WaterCost;
+        public int FoodCost;
+
+        public int StaminaRecoveryBonus;
+        public int WarmthRecoveryBonus;
+        public int MoraleRecoveryBonus;
+    }
+}
