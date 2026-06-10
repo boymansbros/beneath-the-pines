09183334e0c05c2cedc2f69b2934610b88058ad9
boymansbros <boymansbros@gmail.com>
2026-06-10 16:39:19 -0400

    Add GameEnums.cs with various enumerations

---
+++ Assets/Scripts/Core/GameEnums.cs
@@ -0,0 +1,31 @@
+namespace BeneathThePines
+{
+    public enum LoadTier
+    {
+        Light,
+        Balanced,
+        Heavy
+    }
+
+    public enum StatusEffectType
+    {
+        Soaked,
+        Exhausted,
+        Cold
+    }
+
+    public enum WeatherType
+    {
+        Clear,
+        Rain,
+        Fog
+    }
+
+    public enum NodeType
+    {
+        Start,
+        Trail,
+        Camp,
+        Destination
+    }
+}
