cdde8eb0bb0a403a66a31f5bd0259a67e530fc53
boymansbros <boymansbros@gmail.com>
2026-06-11 11:26:17 -0400

    Update NodeDefinition.cs

--- Assets/Scripts/Definitions/NodeDefinition.cs
+++ Assets/Scripts/Definitions/NodeDefinition.cs
@@ -10,9 +10,9 @@ namespace BeneathThePines
 
         public List<string> NextNodeIds = new();
 
-public UnityEngine.Vector2 MapPosition;
+        public UnityEngine.Vector2 MapPosition;
 
-public List<TrailCardDefinition> PossibleCards = new();
+        public List<TrailCardDefinition> PossibleCards = new();
 
         public List<TrailCardDefinition> PossibleCards = new();
     }
